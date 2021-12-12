a730c9003bf5f33a27017572f3d330f3444348e4
Sanket Sanap <sanap.s@northeastern.edu>
2021-12-12 17:03:48 -0500

    created packages and it body

--- Script/Jarvis.sql
+++ Script/Jarvis.sql
@@ -1,6 +1,6 @@
 select USER from dual;
 
-select * from ADMIN.payments;
+select * from ADMIN.car;
 
 SELECT * FROM user_role_privs;
 SELECT * FROM role_sys_privs;
@@ -9,5 +9,3 @@ SELECT * FROM role_tab_privs;
 select owner from ALL_TABLES where TABLE_NAME ='CAR';
 select * from DBA_ROLE_PRIVS where grantee = 'ADMIN';
 select * from ALL_TABLES where OWNER = 'JARVIS';
-
-select * from USE
