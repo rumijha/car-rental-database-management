52f381cc0d6141e36bc45dcf13b30d34d5cfb00f
rumi1jha <rumijha@gmail.com>
2021-12-14 23:48:36 -0500

    DropUserRoles

---
+++ Script/DDL/DropUsersRoles.sql
@@ -0,0 +1,26 @@
+
+--------------------------------------
+--         DROP ROLES
+--------------------------------------
+    DROP ROLE APP_OWNER;
+    DROP ROLE DEVELOPER;
+    DROP ROLE TESTER;
+    DROP ROLE ANALYST;
+    DROP ROLE SERVICE;
+    DROP ROLE SECURITY;
+
+----------------------------------------
+--             DROP USERS
+----------------------------------------
+    --APP_OWNER
+    DROP USER JARVIS; 
+    --DEVELOPER
+    DROP USER HARRY;
+    --TESTER
+    DROP USER PETER;
+    --ANALYST
+    DROP USER TOM;
+    --SERVICE
+    DROP USER ROOT;
+    --AUDIT
+    DROP USER JESSY;
