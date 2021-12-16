18d058d058fd0d38371d2c7946b566a3b5b2c0f1
Vignesh Kumar <vignesh.viki.kumar@gmail.com>
2021-12-15 23:32:26 -0500

    Updated Sample DML Test data

--- DML/sample_DML.sql
+++ DML/sample_DML.sql
@@ -3,7 +3,7 @@ set serveroutput on;
 begin
     --INSERT CUSTOMER
     -- customer_id, first_name, last_name, email, contact, license, passport,cust type, DOB
-    --pckg_customer.insert_customer(786,'Ganesh','Nayak','ganesh.n@northeastern.edu','3456987098','ZXCVB542','LKJHGMN678','EMPLOYEE','02-JUL-1997');
+    --pckg_customer.insert_customer(786,'Ganesh','Nayak','ganesh.n@northeastern.edu',3456987098,'ZXCVB542','LKJHGMN678','EMPLOYEE','02-JUL-1997');
     
     
     --CUSTOMER ADDRESS
@@ -20,7 +20,7 @@ begin
     
     
     --INSERT ACTIVITY activity id, login time, logout time, cust id
-       -- pckg_customer.insert_activity(12000, sysdate, sysdate+1/24, 786);
+      --pckg_customer.insert_activity(12000, sysdate, sysdate+1/24, 786);
     
     
     --INSERT CAR car id, type, seat capacity, rate, model, make,reg num, cost
@@ -39,37 +39,34 @@ begin
         --pckg_car.update_pickup_point(10, 'TEXAS','AUSTIN','92220', 50000, 'AUSTIN_TX');
     
     
-    --INSERT CARS AT PICK UP
-        --pckg_car.insert_cars_at_pickup(15,7012,1047);
-        
-        
-     --UPDATE CARS AT PICK UP
-        --pckg_car.update_cars_at_pickup(15, 7012, 1041);
-        
-        
-        
-        
-        
+    --INSERT CARS AT PICK UP 
+        --pckg_car.insert_cars_at_pickup(15,10,500);
+             
+               
     -- DELETE CAR
-        --pckg_car.delete_car(5);
+        --pckg_car.insert_car(600, 'SEDAN', 4, 16, 'CITY', 'HONDA','QQZ D21', 15800);
+        --pckg_car.delete_car(600);
         
     
     --INSERT RIDE Transaction -  Trans id, cust id, car at pickup id, start, end, car id, distance, status
-        --pckg_transact.insert_ride_transaction(12,4000,9010,to_timestamp((sysdate)), to_timestamp((sysdate+1/24)),1016,0,'IN PROCESS');
+        --pckg_transact.insert_ride_transaction(17, 786, 15, to_timestamp((sysdate)), to_timestamp((sysdate+1/24)),500, 0, 'IN PROCESS');
     
     --UPDATE  Ride Transaction- Trans id, car at pickup id, start, end, car id, distance, status, customer card, discount id
-        --pckg_transact.update_ride_transaction(10,9010,to_timestamp((sysdate+1/24)), to_timestamp((sysdate+2/24)),1018,10,'COMPLETED','1234-1234-1234-1234',NULL);
+       --pckg_transact.update_ride_transaction(17, 15, to_timestamp((sysdate+1/24)), to_timestamp((sysdate+2/24)), 500, 0,'UPDATED',NULL, NULL);
+       
+    --COMPLETE RIDE TRANSACTION- Trans id, car at pickup id, start, end, car id, distance, status, customer card, discount id
+       --pckg_transact.update_ride_transaction(17, 15, to_timestamp((sysdate+1/24)), to_timestamp((sysdate+2/24)), 500, 26,'COMPLETED','1234-1234-1234-1234',NULL);
+    
+    --INSERT violations- violation id description
+        --pckg_violations.insert_violations(22,'Traffic Signal Jump');
+        --pckg_violations.update_violations(22,'Signal Jump');
+    
+    --INSERT VIOLATIONS records - record id,  cust id, violation id
+      --pckg_violations.insert_violations_record(2000,786,22);
         
+    --SHOW ALL AVAILABLE CARS - start time, end time    
+        --pckg_utils.show_all_available_cars(to_timestamp((sysdate)), to_timestamp((sysdate+2/24)));
     
-    --INSERT violations violation id description
-        --pckg_violations.insert_violations(22,NULL);
-        --pckg_violations.update_violations(21,'Park');
-    
-    --INSERT VIOLATIONS records record id,  cust id, violation id
-      -- pckg_violations.insert_violations_record(2000,2007,12);
-        
-    --INSERt
-    
-    --pckg_utils.show_all_available_cars(to_timestamp((sysdate)), to_timestamp((sysdate+2/24)));
-    --pckg_utils.get_car(1009);
+    --GET CAR - car id
+        --pckg_utils.get_car(500);
 end;
